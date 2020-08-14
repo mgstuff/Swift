9b625c1cc1b77feb22b5f5e49661ab447ba1bae8
Michał Grycki <Micha Grycki>
2020-08-14 08:57:06 +0200

    First Commit

---
+++ proj2/proj2/ContentView.swift
@@ -0,0 +1,70 @@
+//
+//  ContentView.swift
+//  proj2
+//
+//  Created by Michał Grycki on 12/08/2020.
+//  Copyright © 2020 Michał Grycki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+
+    
+    var body: some View
+    {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
+        
+        VStack(spacing: 30)
+        {
+            VStack {
+                Text("Tap the flag of").foregroundColor(.white)
+                Text(countries[correctAnswer]).font(.largeTitle)
+                .fontWeight(.black)
+            }
+
+            ForEach(0 ..< 3) { number in
+                Button(action: {
+                    self.flagTapped(number)
+                }) {
+                    Image(self.countries[number])
+                        .renderingMode(.original).clipShape(Capsule())
+                }
+            }
+        }
+    }.alert(isPresented: $showingScore) {
+        Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+            self.askQuestion()
+        })
+    }
+}
+    
+    func flagTapped(_ number: Int)
+    {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }}
+}
